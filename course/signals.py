67310f1fd34f1d5e92af9c76f61d9bcbe61deee6
Dennis Kamau Ngera <ngerakam123@gmail.com>
2024-08-01 15:51:11 +0300

    added a task for sending enrollment email course and related template 01

--- course/signals.py
+++ course/signals.py
@@ -1,10 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Enrollment
-from .tasks import send_course_enrollment_email
+from .tasks import send_course_enrollment_email, create_progress
 
 @receiver(post_save, sender=Enrollment)
 def create_progress_signal(sender, instance, created, **kwargs):
+    if created:
+        create_progress.delay(instance.id)
+
+@receiver(post_save, sender=Enrollment)
+def send_progress_email_signal(sender, instance, created, **kwargs):
     user = instance.user.id
     course = instance.course.id
     if created:
