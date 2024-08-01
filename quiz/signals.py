46489af70a7dd3441faa4e789e226b01bdb27a93
Dennis Kamau Ngera <ngerakam123@gmail.com>
2024-08-01 18:18:47 +0300

    added asyncResult for getting results from celery 05

--- quiz/signals.py
+++ quiz/signals.py
@@ -1,17 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import EssayGrade, EssayQuestionAnswer
-from .tasks import calculate_quiz_score
+from .tasks import run_calculate_quiz_score
 
 @receiver(post_save, sender=EssayGrade)
 def regrade_essay(sender, instance, created, **kwargs):
     if not created and instance.score is not None:
-        calculate_quiz_score.delay(instance.quiz_attempt.id)
-
-
-@receiver(post_save, sender=EssayGrade)
-def recalculate_quiz_score_on_essay_grade_update(sender, instance, **kwargs):
-    if not kwargs.get('created') and instance.score is not None:
-        instance.quiz_attempt.score_calculated = False
-        instance.quiz_attempt.save(update_fields=['score_calculated'])
-        calculate_quiz_score.apply_async(countdown=10, args=[instance.quiz_attempt.id])
+        run_calculate_quiz_score.delay(instance.quiz_attempt.id)
