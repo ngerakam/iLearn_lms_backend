36bb2ecd3808b4b90bc207860f77009d6feb3fa5
Dennis Kamau Ngera <ngerakam123@gmail.com>
2024-08-07 18:18:51 +0300

    added email setting for ssl

--- ilearn_backend/settings/prod.py
+++ ilearn_backend/settings/prod.py
@@ -17,7 +17,8 @@ DATABASES = {
 
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 25))  # Default to port 25 if not specified
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == 'True'  # Convert string to boolean
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == 'True'  # Convert string to boolean
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
