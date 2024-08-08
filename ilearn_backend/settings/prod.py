2f91f40060b0d8d9f7deba96e665761ac56665d2
Dennis Kamau Ngera <ngerakam123@gmail.com>
2024-08-08 12:53:36 +0300

    added email setting for ssl 02

--- ilearn_backend/settings/prod.py
+++ ilearn_backend/settings/prod.py
@@ -15,20 +15,30 @@ DATABASES = {
     }
 }
 
+import os
+
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == 'True'
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == 'True'
 
 if EMAIL_USE_SSL:
-    EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
-    EMAIL_HOST = os.getenv("EMAIL_HOST")
-    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 25))
-    EMAIL_USE_SSL = EMAIL_USE_SSL
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
+    EMAIL_USE_TLS = False  # Ensure TLS is not used if SSL is enabled
 elif EMAIL_USE_TLS:
-    EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
-    EMAIL_HOST = os.getenv("EMAIL_HOST")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+    EMAIL_USE_SSL = False  # Ensure SSL is not used if TLS is enabled
+else:
     EMAIL_PORT = int(os.getenv("EMAIL_PORT", 25))
-    EMAIL_USE_TLS = EMAIL_USE_TLS
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_SSL = False
+    EMAIL_USE_TLS = False
+
+# Make sure EMAIL_USE_TLS or EMAIL_USE_SSL is set correctly
+if EMAIL_USE_TLS and EMAIL_USE_SSL:
+    raise ValueError('EMAIL_USE_TLS and EMAIL_USE_SSL are mutually exclusive; only one should be True.')
+
+# Set Django email settings
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 25))
